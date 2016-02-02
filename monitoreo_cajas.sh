f8b3ef78492d80c2efa1e49b7fe2ccd5d326b1f1
Ivan David Barron Baez <ivandavid77@gmail.com>
2016-02-02 11:49:59 -0600

    actualizacion de scripts de monitoreo

---
+++ monitoreo_cajas.sh
@@ -0,0 +1,1 @@
+watch -n 15 -c "cat cajas.json | ~/node4.1.0/bin/node ~/node4.1.0/lib/node_modules/underscore-cli/bin/underscore print"
---
+++ monitoreo_turnos.sh
@@ -0,0 +1,1 @@
+watch -n 5 -c "cat turnos.json | ~/node4.1.0/bin/node ~/node4.1.0/lib/node_modules/underscore-cli/bin/underscore print"
